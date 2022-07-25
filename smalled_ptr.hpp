d7f30360f0fe5b2d42106783e8371054c842ebb7
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2022-07-25 23:10:48 +0200

    remove unnecessary casts

--- smalled_ptr.hpp
+++ smalled_ptr.hpp
@@ -10,23 +10,18 @@ template <class T> struct hidden_value_ptr {
   explicit hidden_value_ptr(T *pointer)
       : _pointer(reinterpret_cast<std::uintptr_t>(pointer)) {}
 
-  T *get() const {
-    const auto p = reinterpret_cast<std::uintptr_t>(_pointer);
-    return reinterpret_cast<T *>(p & ~1ul);
-  }
+  T *get() const { return reinterpret_cast<T *>(_pointer & ~1ul); }
 
   bool flag() const {
-    const auto p = reinterpret_cast<std::uintptr_t>(_pointer);
-    const auto p2 = (p & 1ul);
+    const auto p2 = (_pointer & 1ul);
     return p2 == 1ul;
   }
 
   void flag(bool value) {
-    const auto p = reinterpret_cast<std::uintptr_t>(_pointer);
     if (value)
-      _pointer = p | 1ul;
+      _pointer = _pointer | 1ul;
     else
-      _pointer = p & ~1ul;
+      _pointer = _pointer & ~1ul;
   }
 
 private:
