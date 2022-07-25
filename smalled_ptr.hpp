96549874f93e81661ec17d9d0c32b09248de5b0b
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2022-07-25 21:56:08 +0200

    typo

--- smalled_ptr.hpp
+++ smalled_ptr.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-/// Reference counter smart pointer, similar to `shared_ptr`, but its size
+/// Reference counted smart pointer, similar to `shared_ptr`, but its size
 /// equals the size of the raw pointer.
 template <class T> struct smalled_ptr {
   smalled_ptr(T *pointer) : _ptr(pointer) {}
